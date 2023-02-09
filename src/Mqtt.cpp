b7b9d935edef1e366a88a11533e5356d503c04fe
Konrad Macialek <konrad.macialek@vonage.com>
2023-02-09 11:36:09 +0100

    Code extracted to separate files

---
+++ src/Mqtt.cpp
@@ -0,0 +1,98 @@
+#include "Mqtt.h"
+
+const char *PubTopic  = "async-mqtt/ESP8266_Pub";
+const char *SubTopic  = "remotecardiagz/activemeasurements";
+
+void connectToMqtt()
+{
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onMqttConnect(bool sessionPresent)
+{
+  Serial.print("Connected to MQTT broker: ");
+  Serial.print(MQTT_HOST);
+  Serial.print(", port: ");
+  Serial.println(MQTT_PORT);
+  Serial.print("PubTopic: ");
+  Serial.println(PubTopic);
+
+  printSeparationLine();
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+
+  uint16_t packetIdSub = mqttClient.subscribe(SubTopic, 2);
+  Serial.print("Subscribing at QoS 2, packetId: ");
+  Serial.println(packetIdSub);
+
+  // uint16_t packetIdPub1 = mqttClient.publish(PubTopic, 1, true, "ESP8266 Test2");
+  // Serial.print("Publishing at QoS 1, packetId: ");
+  // Serial.println(packetIdPub1);
+
+  printSeparationLine();
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
+{
+  (void) reason;
+
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected())
+  {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+void onMqttSubscribe(const uint16_t& packetId, const uint8_t& qos)
+{
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(const uint16_t& packetId)
+{
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, const AsyncMqttClientMessageProperties& properties,
+                   const size_t& len, const size_t& index, const size_t& total)
+{
+  (void) payload;
+
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+  Serial.println("    payload:    ");
+  Serial.print(payload);
+}
+
+void onMqttPublish(const uint16_t& packetId)
+{
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void printSeparationLine()
+{
+  Serial.println("************************************************");
+}
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include <ArduinoJson.h>
 #include <Ticker.h>
 #include <AsyncMqtt_Generic.h>
-
+#include "Mqtt.h"
 // WiFi connection settings
 const char* WIFI_SSID = "VagCan";
 const char* WIFI_PASSWORD = "vagcan1234";
@@ -22,20 +22,10 @@ unsigned int invlTx = 200;
 byte activePids[10];
 int alreadySentIndex = 0;
 
-//my mppt setup
 VagCanMptt MPPT(15);
 RemoteCarDiagzAPI RemoteCarDiagzApi(WiFi);
-
-// MQTT Protocol setup
-#define MQTT_HOST         IPAddress(3, 71, 12 , 183)
-// #define MQTT_HOST         "broker.emqx.io"        // Broker address
-#define MQTT_PORT         1883
-
-const char *PubTopic  = "async-mqtt/ESP8266_Pub";
-const char *SubTopic  = "remotecardiagz/activemeasurements";
 AsyncMqttClient mqttClient;
 Ticker mqttReconnectTimer;
-
 WiFiEventHandler wifiConnectHandler;
 WiFiEventHandler wifiDisconnectHandler;
 Ticker wifiReconnectTimer;
@@ -44,12 +34,6 @@ void connectToWifi()
 {
   Serial.println("Connecting to Wi-Fi...");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-}
-
-void connectToMqtt()
-{
-  Serial.println("Connecting to MQTT...");
-  mqttClient.connect();
 }
 
 void onWifiConnect(const WiFiEventStationModeGotIP& event)
@@ -70,107 +54,10 @@ void onWifiDisconnect(const WiFiEventStationModeDisconnected& event)
   wifiReconnectTimer.once(2, connectToWifi);
 }
 
-void printSeparationLine()
-{
-  Serial.println("************************************************");
-}
-
-void onMqttConnect(bool sessionPresent)
-{
-  Serial.print("Connected to MQTT broker: ");
-  Serial.print(MQTT_HOST);
-  Serial.print(", port: ");
-  Serial.println(MQTT_PORT);
-  Serial.print("PubTopic: ");
-  Serial.println(PubTopic);
-
-  printSeparationLine();
-  Serial.print("Session present: ");
-  Serial.println(sessionPresent);
-
-  uint16_t packetIdSub = mqttClient.subscribe(SubTopic, 2);
-  Serial.print("Subscribing at QoS 2, packetId: ");
-  Serial.println(packetIdSub);
-
-  // uint16_t packetIdPub1 = mqttClient.publish(PubTopic, 1, true, "ESP8266 Test2");
-  // Serial.print("Publishing at QoS 1, packetId: ");
-  // Serial.println(packetIdPub1);
-
-  printSeparationLine();
-}
-
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
-{
-  (void) reason;
-
-  Serial.println("Disconnected from MQTT.");
-
-  if (WiFi.isConnected())
-  {
-    mqttReconnectTimer.once(2, connectToMqtt);
-  }
-}
-
-void onMqttSubscribe(const uint16_t& packetId, const uint8_t& qos)
-{
-  Serial.println("Subscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-  Serial.print("  qos: ");
-  Serial.println(qos);
-}
-
-void onMqttUnsubscribe(const uint16_t& packetId)
-{
-  Serial.println("Unsubscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}
-
-void onMqttMessage(char* topic, char* payload, const AsyncMqttClientMessageProperties& properties,
-                   const size_t& len, const size_t& index, const size_t& total)
-{
-  (void) payload;
-
-  Serial.println("Publish received.");
-  Serial.print("  topic: ");
-  Serial.println(topic);
-  Serial.print("  qos: ");
-  Serial.println(properties.qos);
-  Serial.print("  dup: ");
-  Serial.println(properties.dup);
-  Serial.print("  retain: ");
-  Serial.println(properties.retain);
-  Serial.print("  len: ");
-  Serial.println(len);
-  Serial.print("  index: ");
-  Serial.println(index);
-  Serial.print("  total: ");
-  Serial.println(total);
-  Serial.println("    payload:    ");
-  Serial.print(payload);
-}
-
-void onMqttPublish(const uint16_t& packetId)
-{
-  Serial.println("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}
-
 void setup(){
   Serial.begin(115200);
   while(!Serial);
  
- // Initialize WiFi
-  // WiFi.begin(ssid, password);
-  // while (WiFi.status() != WL_CONNECTED) {
- 
-  //   delay(1000);
-  //   Serial.print("Connecting...");
-  // }
-
- // ****************************
   wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
   wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
 
